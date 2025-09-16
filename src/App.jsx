c085eeff8d592cebc396f1d416d20277df59d1c2
pparedes1 <pparedes1@users.noreply.github.com>
2025-09-15 22:23:27 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Checkout from "./components/Checkout.jsx";
 export default function App() {
   return (
     <div style={{ maxWidth: 720, margin: "2rem auto", fontFamily: "sans-serif" }}>
-      <h1>Consulting with Pablo Paredes</h1>
+      <h1>Consulting with Pedro Paredes</h1>
       <AboutMe />
       <BookTime />
       <Checkout />
